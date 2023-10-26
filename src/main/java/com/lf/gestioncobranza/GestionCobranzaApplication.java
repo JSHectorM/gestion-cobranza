934cf6ee2601537c457d978205e6a9bc7ef10ebb
Hector MJS <miguel.lin123@gmail.com>
2023-10-26 12:27:22 -0600

    first commit

---
+++ src/main/java/com/lf/gestioncobranza/GestionCobranzaApplication.java
@@ -0,0 +1,13 @@
+package com.lf.gestioncobranza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCobranzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionCobranzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lf/gestioncobranza/GestionCobranzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lf.gestioncobranza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCobranzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
