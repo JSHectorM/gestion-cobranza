6979bc0c74ac7e085dc2f512a2dec88de66d4066
Hector MJS <miguel.lin123@gmail.com>
2023-10-26 17:10:25 -0600

    add repository fianza y Pagopendiente

---
+++ src/main/java/com/lf/gestioncobranza/dao/FianzaRepository.java
@@ -0,0 +1,8 @@
+package com.lf.gestioncobranza.dao;
+
+import com.lf.gestioncobranza.model.Fianza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FianzaRepository extends JpaRepository<Fianza, Long> {
+
+}
---
+++ src/main/java/com/lf/gestioncobranza/dao/PagoPendienteRepository.java
@@ -0,0 +1,8 @@
+package com.lf.gestioncobranza.dao;
+
+import com.lf.gestioncobranza.model.PagoPendiente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagoPendienteRepository extends JpaRepository<PagoPendiente, Long> {
+
+}
