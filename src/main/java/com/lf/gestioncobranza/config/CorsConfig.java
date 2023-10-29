cfbc7f06244f18007a176d22db6e70d39288a628
Hector JS <58092446+JSHectorM@users.noreply.github.com>
2023-10-29 11:20:57 -0600

    add class corsconfig

---
+++ src/main/java/com/lf/gestioncobranza/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.lf.gestioncobranza.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
